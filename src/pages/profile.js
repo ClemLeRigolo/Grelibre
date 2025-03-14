b776fe4535eec00f21a1e685d6007f8a33cc72a6
Clem <clement.correnoz@grenoble-inp.org>
2025-03-14 15:30:33 +0100

    petit profil

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
-import MapComponent from '../components/MapComponent';
-import '../styles/home.css';
-
+import { UserAvatarFilledAlt } from "@carbon/icons-react";
+import { Button, Tile } from "@carbon/react";
+import { Link } from "react-router-dom";
 import { authStates, withAuth } from "../components/auth";
 import { signOut } from "../utils/firebase";
 import Loader from "../components/loader";
-import { Link } from "react-router-dom";
+import "../styles/profile.css";
 
 function handleSignOut() {
   signOut()
@@ -25,26 +24,25 @@ class Profile extends React.Component {
     }
 
     if (this.props.authState === authStates.LOGGED_OUT) {
-        const currentUrl = window.location.pathname;
-        return (
-            <div style={
-                {
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh',
-                    flexDirection: 'column'
-                }
-            }>
-              <p>Vous devez vous connecter pour accéder à cette page</p>
-              <Link className="button" to={`/login?redirect=${currentUrl}`}>Se connecter</Link>
-            </div>
-          );
+      const currentUrl = window.location.pathname;
+      return (
+        <div className="profile-unauthorized">
+          <p>Vous devez vous connecter pour accéder à cette page</p>
+          <Link className="button" to={`/login?redirect=${currentUrl}`}>Se connecter</Link>
+        </div>
+      );
     }
 
     return (
-      <div className="home-container">
-        
+      <div className="profile-container">
+        <Tile className="profile-card">
+          <UserAvatarFilledAlt size={48} />
+          <h2>{this.props.user?.displayName || "Utilisateur"}</h2>
+          <p>{this.props.user?.email || "Email non disponible"}</p>
+          <Button onClick={handleSignOut}>
+            Se déconnecter
+          </Button>
+        </Tile>
       </div>
     );
   }
